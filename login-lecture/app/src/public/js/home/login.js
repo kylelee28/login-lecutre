9ce927fb31001976dbbf8b67bbd72b0c98fa7b54
kylelee28 <successor1119@naver.com>
2023-10-20 23:50:26 +0900

    돔을 이용하여 html 객체 저장

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict"
 
-console.log("hello");
-console.log("bye")
+// DOM -> Document object module
+// java에서 html 내용을 제어한다. 
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+
+// id에 들어가서 태그의 value에 접근
+
+function login() {
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+}
+
