959772046a7037b32ebff3a82b413c08ed33500f
kylelee28 <successor1119@naver.com>
2023-10-23 09:56:37 +0900

    백엔드에서 로그인 기능 구현

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,7 +1,8 @@
 "use strict"
 
 // DOM -> Document object module
-// java에서 html 내용을 제어한다. 
+// java에서 html 내용을 제어한다.
+
 
 const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
@@ -17,14 +18,20 @@ function login() {
         psword : psword.value,
     };
 
+// 모든 각 언어마다 지원하고 있는 데이터 타입이 다르다.
+// 모두 호환할 수 있는 데이터 체계 => json
+// 문자열로 전송되기 때문에 데이터 크기 줄어들음.
+// 이해도 쉬움. 예전엔 XML이용.
 
     fetch("./login", {
         method : "POST",
-        header : {
+        headers : {
             "Content-Type" : "application/json"
         },
-        body : JSON.stringyfy(req),
-    });
+        body : JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then(console.log);
 }
 
 
--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -1,15 +1,44 @@
 "use strict";
 
 
-const home = (req, res) => {
-    res.render("home/index")
-};
-
-const login = (req, res) => {
-    res.render("home/login")
+const output = {
+    home : (req, res) => {
+        res.render("home/index")
+    },
+    
+    login : (req, res) => {
+        res.render("home/login")
+    },
 }
 
+const users = {
+    id : ["이재은", "손원진", "오은재"],
+    psword : ["1234", "123", "12"],
+};
+
+const process = {
+    login : (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+            console.log(id, psword);
+            if(users.id.includes(id)) {
+               const idx = users.id.indexOf(id);
+               if (users.psword[idx] === psword) {
+                return res.json({
+                    success : true,
+                });
+               }
+            }
+            return res.json({
+                success : false,
+                msg : "로그인에 실패하셨습니다.",
+            })
+    },
+};
+    
+
+
 module.exports = {
-    home, 
-    login,
+    process,
+    output,
 }; 
